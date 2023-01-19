7cb7815176ad87c71d5d1fe1af67cd8051bd39b2
Ioana56 <116362539+Ioana56@users.noreply.github.com>
2023-01-19 17:05:48 +0200

    Add files via upload

---
+++ Authors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog_Remastered.Models
+{
+    [Index(nameof(Username), IsUnique = true)]
+    public class Authors
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string About { get; set; } = string.Empty;
+        public string ApplicationUserId { get; set; }
+    }
+}
