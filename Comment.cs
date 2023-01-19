930e81389d593050a37ddef8be64e7767ca91654
Ioana56 <116362539+Ioana56@users.noreply.github.com>
2023-01-19 17:06:10 +0200

    Add files via upload

---
+++ Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog_Remastered.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Body { get; set; }
+        public Authors Author { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime Deleted { get; set; }
+
+        [NotMapped]
+        public int? PostIdFromQuery { get; set; }
+    }
+}
