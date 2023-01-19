1cdef5e94b93566780b86d68be9d1e29094e6785
Ioana56 <116362539+Ioana56@users.noreply.github.com>
2023-01-19 16:56:47 +0200

    Add files via upload

---
+++ DataContext.cs
@@ -0,0 +1,26 @@
+﻿using Blog_Remastered.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog_Remastered.Data;
+
+public class DataContext : IdentityDbContext<IdentityUser>
+{
+    public DataContext(DbContextOptions<DataContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Comment> comments => Set<Comment>();
+    public DbSet<Post> posts => Set<Post>();
+    public DbSet<Authors> authors => Set<Authors>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
